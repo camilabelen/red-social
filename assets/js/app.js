3f15c2d66ed1910859201c6736dbebc3a533987e
SabrinaVillalobos <wafflish@gmail.com>
2018-01-04 20:17:19 -0300

    follow unfollow

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,9 @@ $(".pageini").hide()
 $(".navbarcontainer").hide()
 $(".menuinf").hide()
 $(".addcontact").hide()
+$(".unfollowbtn").hide()
+$(".unfollowbtn1").hide()
+$(".unfollowbtn2").hide()
 
 $(document).ready(function(){
   $(".login").show()
@@ -48,6 +51,37 @@ $(".homemobile").click(function(){
   $(".addcontact").hide()
   $(".pageini").show()
 })
+
+$(".followbtn").click(function(){
+  $(".unfollowbtn").show()
+  $(".followbtn").hide()
+})
+
+$(".unfollowbtn").click(function(){
+  $(".followbtn").show()
+  $(".unfollowbtn").hide()
+})
+
+
+$(".followbtn1").click(function(){
+  $(".unfollowbtn1").show()
+  $(".followbtn1").hide()
+})
+
+$(".unfollowbtn1").click(function(){
+  $(".followbtn1").show()
+  $(".unfollowbtn1").hide()
+})
+
+$(".followbtn2").click(function(){
+  $(".unfollowbtn2").show()
+  $(".followbtn2").hide()
+})
+
+$(".unfollowbtn2").click(function(){
+  $(".followbtn2").show()
+  $(".unfollowbtn2").hide()
+})
  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyAd7xNUJssjA3v-i8zDfhpY6Lg3R5MGQy8",
@@ -56,7 +90,6 @@ $(".homemobile").click(function(){
     projectId: "fine-climber-188103",
     storageBucket: "fine-climber-188103.appspot.com",
     messagingSenderId: "693407936885"
-
   };
   firebase.initializeApp(config);
 
@@ -138,16 +171,16 @@ $(".navbarcontainer").show()
 }
 
 $(".logout").click(function(){
-firebase.auth().signOut()
-.then(function(){
-console.log("saliendo...")
-})
-.catch(function(error){
+  firebase.auth().signOut()
+  .then(function(){
+    console.log("saliendo...")
+  })
+  .catch(function(error){
 console.log(error)
 })
-$(".pageini").hide();
-$(".inicio").show();
-$(".navbarcontainer").hide()
+  $(".pageini").hide();
+  $(".inicio").show();
+  $(".navbarcontainer").hide()
 });
 }
 
@@ -167,11 +200,15 @@ user.sendEmailVerification().then(function() {
 //modal para subir archivos
 
 //Añadir comentarios Comentarios
-  $('#comment').keyup(function(e){
+  $('.comment').keyup(function(e){
     if(e.keyCode == 13){
-      var newComment = $('#comment').val();
-      $('.allComments').append('<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 newComment">'+ '<p class="col-lg-10">' + newComment + '</p>' + '<span class="col-lg-1 glyphicon glyphicon-heart">' + '</span>' + '<span class="col-lg-1 glyphicon glyphicon-trash">' + '</span>' +'</div>');
-      $('#comment').val('');
+      var newComment = $(this).val();
+      $('<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 newComment">' 
+        + '<p class="col-lg-10">' + newComment + '</p>'
+        + '<span class="col-lg-1 glyphicon glyphicon-heart">' + '</span>' 
+        + '<span class="col-lg-1 glyphicon glyphicon-trash">' + '</span>' 
+        +'</div>').insertBefore(this).closest('div');
+      $(this).val('');
       $('.newComment').hide();
     }
   });
@@ -179,6 +216,53 @@ user.sendEmailVerification().then(function() {
   $('.allComments p').click(function(){
     $('.newComment').show();
   });
+  //eliminar comentarios
+  $('.glyphicon-trash').click(function(){
+    $(this).parent().remove();
+  });
+  //subir imagen
+  $('#imageUpload').change(function(){
+    readImgUrlAndPreview(this);
+    function readImgUrlAndPreview(input){
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $('.uploadImg').append('<img src="" id="imagePreview" alt="" width="5em"/>');                 
+          $('#imagePreview').attr('src', e.target.result);
+          $('#imagePreview').css('marginTop','1%');
+          $('#imagePreview').css('paddingTop','3%');
+          $('#imagePreview').css('paddingBottom', '8%');
+          $('.uploadImg').css('border','1px solid #CBCBCB');
+          $('.uploadImg').prepend('<button type="button" class="close deleteImg" aria-label="Close">' + '<span aria-hidden="true">' + '&times;' + '</span>');
+          $('.uploadImg').css('marginTop', '3%')
+        }
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  });
 
+   //Borrar imagen
+  $('.deleteImg').click(function(){
+     
+   });
+  //Hacer publicación
+  $('.publish').click(function(){
+    var newPost = $('.addPost').val();
+    var img = $('#imagePreview');
+    $('.allPost').prepend('<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12 col-sm-offset-1 post">' 
+                      + '<div class="postAuthor">' + '<div class="col-lg-1 col-xs-2 photoProfile">' 
+                      + '<img src="assets/img/gato5.jpg">' + '</div>' + '<div class="col-lg-10 name">' 
+                      + '<h5>' + '<strong>' + 'Pelusa' + '</strong>' + '</h5>' + '</div>' + '</div>' 
+                      + '<div>' + '<img src='+img[0].src+'>' + '</div>' + '<div class="col-lg-12">' 
+                      + '<span class="glyphicon glyphicon-heart-empty like">' + '</span>' + '</div>' 
+                      + '<div class="col-lg-12 col-xs-12">' + '<p>' + '<strong>' + 'Pelusa ' 
+                      + '</strong>' + newPost + '</p>' + '</div>' 
+                      + '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 allComments">' 
+                      + '<p class="showComment">' + '<strong>' + 'Mostrar Comentarios...' 
+                      + '</strong>' + '</p>' + '</div>' + '<div class="col-lg-12 col-xs-12">' 
+                      + '<div class="form-group">'
+                      + '<textarea class="form-control comment" rows="1" placeholder="Escribe un comentario...">' 
+                      + '</textarea>' + '</div>' + '</div>' + '</div>');
+    $('.addPost').val('');
+  });
 });
-
